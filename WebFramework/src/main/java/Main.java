50a69f40eb919509c9b55e1dc7c679ef988aad06
Paul Zharskiy <paulzharskiy@gmail.com>
2018-12-14 21:00:47 +0300

    Second commit

--- WebFramework/src/main/java/Main.java
+++ WebFramework/src/main/java/Main.java
@@ -4,13 +4,12 @@ import util.Checker;
 public class Main {
 
     public static void main(String[] args) {
-        String[] arg = {"src/main/resources/test.txt", "src/main/resources/log.txt"};
         Checker checker = new Checker();
         Commands commands = new Commands();
         try {
-            checker.checkConsoleArguments(arg);
-            commands.readFromFile(arg[0]);
-            commands.setLogFile(arg[1]);
+            checker.checkConsoleArguments(args);
+            commands.readFromFile(args[0]);
+            commands.setLogFile(args[1]);
             commands.execute();
             commands.logToFile();
         } catch (Exception e) {
